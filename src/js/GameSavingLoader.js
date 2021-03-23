cb8276147fa94db4e895635a854ce00fd831c20c
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-03-23 21:48:56 +0300

    fix

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -3,14 +3,6 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static load() {
-    return new Promise((resolve) => {
-      const data = read();
-      data.then((response) => {
-        const value = json(response);
-        return value;
-      }).then((value) => {
-        resolve(value);
-      });
-    });
+    return read().then((res) => json(res));
   }
 }
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-named-as-default
 import GameSavingLoader from '../GameSavingLoader';
 
 test('promise', async () => {
