4daa3656afb1a9133b871384cd650d7677f20b30
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-04-04 18:05:09 +0300

    fix

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,8 +1,9 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static load() {
-    return read().then((res) => json(res)).then((res) => res);
+    return read().then((res) => json(res)).then((res) => new GameSaving(res));
   }
 }
